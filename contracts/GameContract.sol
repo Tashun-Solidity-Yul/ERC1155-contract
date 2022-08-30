3bc8d1ba0d08b7e4b2d8ffb5ae7376ae41a51309
tashun <tashun.chamara@gmail.com>
2022-08-30 23:13:58 +0530

    to do

--- contracts/GameContract.sol
+++ contracts/GameContract.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./BaseGameControllerContract.sol";
 
-
+// todo add cool down
 contract GameContract is BaseGameControllerContract {
 
 
